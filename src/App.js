6b1d8ed41f8b82b4e11fd4c1e78d2e2f3c05b773
Vadim Smagin <magenvadim@gmail.com>
2023-12-19 03:32:22 +0200

    #3 Country component has added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Header from './Components/Header'
 import Search from './Components/Search'
-
+import Country from './Components/Country'
 
 function App() {
   return (
+
     <div className="app">
+
       <Header/>
+
       <div className="app_body">
         <Search/>
       </div>
+
+      <Country/>
       
     </div>
+
   );
 }
 
