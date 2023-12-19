a97b324cd5d6222ba8c73d0665786f29187b234f
Vadim Smagin <magenvadim@gmail.com>
2023-12-20 01:39:17 +0200

    #4 CountryDetails component has added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './Components/Header'
 import Search from './Components/Search'
 import Country from './Components/Country'
+import CountryDetails from './Components/CountryDetails'
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -9,12 +11,24 @@ function App() {
     <div className="app">
 
       <Header/>
+      
+      <Routes>
+        <Route path='' element={
+          <>
+            <div className="app_body">
+              <Search/>
+            </div>
+      
+            <div className="countries">
+              <Country/>
+            </div>
+          </>
+        }/>
+      </Routes>
 
-      <div className="app_body">
-        <Search/>
-      </div>
-
-      <Country/>
+      <Routes>
+        <Route path='country-details' element={<CountryDetails/>}/>
+      </Routes>
       
     </div>
 
---
+++ src/Components/CountryDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryDetails() {
+  return (
+    <div className='country_details'>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default CountryDetails
