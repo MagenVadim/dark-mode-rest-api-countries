8244e64d4713b8c9a91f1a1d325d98825691a7a3
Vadim Smagin <magenvadim@gmail.com>
2023-12-20 21:23:48 +0200

    #5 CountryDetails styles has added

--- src/Components/CountryDetails.js
+++ src/Components/CountryDetails.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function CountryDetails() {
   return (
     <div className='country_details'>
-      <h1>Test</h1>
+
+      <button className="back">
+        <ArrowBackIcon/>
+        <p>Go Back</p>
+      </button>
+
+      <div className="country_details_body">
+        <div className="img_container">
+            <img src="https://flagcdn.com/as.svg" alt="" />
+        </div>
+        <div className="info">
+            <h2>Name</h2>
+            <div className="info_container">
+                <div className="left_info">
+                    <p>Native Name: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Population: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Region: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Sub Region: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Capital: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                </div>
+
+                <div className="right_info">
+                    <p>Top Level Domain: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Currencies: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                    <p>Languages: {" "}
+                        <span className="values">Test</span>
+                    </p>
+                </div>
+            </div>
+                Border Countries:
+            <div className="border_country">
+                <p>Test</p>
+            </div>
+            <div className="border_country">
+                <p>Test</p>
+            </div>
+            <div className="border_country">
+                <p>Test</p>
+            </div>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
